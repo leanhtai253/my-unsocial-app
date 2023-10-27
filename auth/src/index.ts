89f2eb941e6a0555c904c194df511dc870f1195c
Tai Le <leanhtai253@gmail.com>
2023-10-27 13:44:14 +0700

    9

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,11 +1,5 @@
-import express from 'express';
-
-const app = express();
-
-app.get("*", (req, res) => {
-  res.send({});
-});
+import { app } from './app';
 
 app.listen(3000, () => {
-  console.log("Listening on port 3000.");
+  console.log('Listening on port 3000.');
 });
